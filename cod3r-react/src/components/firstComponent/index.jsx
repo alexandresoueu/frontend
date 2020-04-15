0bfe3e1d78752dedb43c8cb509cf6434dd2db58f
calexandredev <calexandredev@gmail.com>
2020-04-15 18:47:37 -0300

    FirstComponent

---
+++ cod3r-react/src/components/firstComponent/index.jsx
@@ -0,0 +1,7 @@
+import React, {Fragment} from 'react'
+
+export default props =>
+  <Fragment>
+    <h1>SALVE RAPEIZE!</h1>
+    <h2>{props.name}</h2>
+  </Fragment>
---
+++ cod3r-react/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FirstComponent from './components/firstComponent'
+
+ReactDOM.render(<FirstComponent name="Alexandre"/>, document.getElementById('root'))
