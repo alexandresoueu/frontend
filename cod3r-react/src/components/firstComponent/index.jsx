e1999c7a92a9094b693d6171024a0eea3326a3b0
calexandredev <calexandredev@gmail.com>
2020-04-16 10:25:47 -0300

    examples of show more than one elements

--- cod3r-react/src/components/firstComponent/index.jsx
+++ cod3r-react/src/components/firstComponent/index.jsx
@@ -1,7 +1,25 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 
 export default props =>
   <Fragment>
     <h1>SALVE RAPEIZE!</h1>
     <h2>{props.name}</h2>
   </Fragment>
+
+/**
+ * <React.Fragment>
+    <h1>SALVE RAPEIZE!</h1>
+    <h2>{props.name}</h2>
+  </React.Fragment>
+
+  export default props =>[
+    <h1>SALVE RAPEIZE!</h1>
+    <h2>{props.name}</h2>
+  ]
+
+  export default props =>
+  <div>
+    <h1>SALVE RAPEIZE!</h1>
+    <h2>{props.name}</h2>
+  </div>
+ */
