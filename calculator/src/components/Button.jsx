2bfa8c0ca7701c65be202d7bdb5943528195afc3
calexandredev <calexandredev@gmail.com>
2020-04-24 16:43:53 -0300

    create css and catch the element clicked on the button

--- calculator/src/components/Button.jsx
+++ calculator/src/components/Button.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 import './Button.css'
 
-export default props =>
-<button className='button'>{props.label}</button>
+export default props => {
+  let styleButtons = 'button '
+
+  styleButtons += props.operation ? 'operation' : ''
+  styleButtons += props.double ? 'double' : ''
+  styleButtons += props.triple ? 'triple' : ''
+
+  return (
+    <button
+      onClick={e => props.click && props.click(props.label)}
+      className={styleButtons}
+    >
+
+      {props.label}
+    </button>
+  )
+}
