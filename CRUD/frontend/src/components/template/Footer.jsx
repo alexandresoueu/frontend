d214f4c264df2a9a1ae5a3fab054a826b7edeea2
calexandredev <calexandredev@gmail.com>
2020-05-24 01:46:56 -0300

    create content and style

--- CRUD/frontend/src/components/template/Footer.jsx
+++ CRUD/frontend/src/components/template/Footer.jsx
@@ -3,5 +3,6 @@ import './Footer.css'
 
 export default props => 
     <footer className="footer">
-        Footer
+        Desenvolvido com <i className="fa fa-heart text-danger"></i> por 
+        <strong> Cha<span className="text-danger">pp</span>ola</strong>
     </footer>
